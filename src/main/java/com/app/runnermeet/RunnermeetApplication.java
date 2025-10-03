2079156dcbc708a7aa83b629df7654e4bb404d78
abhishekjohri <abhishekjohri098@gmail.com>
2025-10-03 17:40:11 +0530

    Created a new Runner Meetup App Spring MVC project

---
+++ src/main/java/com/app/runnermeet/RunnermeetApplication.java
@@ -0,0 +1,13 @@
+package com.app.runnermeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnermeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnermeetApplication.class, args);
+	}
+
+}
